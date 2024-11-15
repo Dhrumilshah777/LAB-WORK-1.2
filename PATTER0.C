7901834da1128c9b60289816c0e4839441a25002
Dhrumil Shah <dhrumilshah71011@gmail.com>
2024-11-15 18:01:23 +0530

    first commit

---
+++ PATTER0.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+
+void main()
+
+{
+ clrscr();
+ printf("\n ------");
+ printf("\n|C     |");
+ printf("\n|R     |");
+ printf("\n|7     |");
+ printf("\n-------");
+
+
+ getch();
+
+
+}
---
+++ SUM1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+int a = 5;
+int b = 5;
+clrscr();
+printf("Addition of a+b = %d", a+b);
+printf("\n Subtraction of a-b = %d", a-b);
+printf("\n Multiplication of a*b = %d ", a*b);
+printf("\nDivision of a/b = %d ", a/b);
+printf("\nModulos of a%b =  ", a%b);
+
+getch
+();
+
+
+
+
+}
